48608c1fdf8d5fd100295f94ef664445726030b3
Quach Thien Phu <quachphuwork@gmail.com>
2024-04-12 21:37:19 -0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -10,10 +10,10 @@ else:
 import streamlit as st
 import altair as alt
 import pandas as pd
-import seaborn as sns 
-import numpy as np
-import matplotlib as plt 
-import matplotlib.pyplot as plt
+# import seaborn as sns 
+# import numpy as np
+# import matplotlib as plt 
+# import matplotlib.pyplot as plt
 
 from PIL import Image
 from streamlit_option_menu import option_menu
